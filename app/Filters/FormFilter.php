a33890a5a6c6696845b4dcc0f3f3bbc4502b26e6
Valters Garancis <valters.garancis@inbox.lv>
2025-04-28 12:02:12 +0300

    File saving rework, frontend.

--- app/Filters/FormFilter.php
+++ app/Filters/FormFilter.php
@@ -52,7 +52,7 @@ class FormFilter implements FilterInterface
     public function getPremissions($table_name, $user)
     {
         $db = db_connect();
-        $table = $db->query("SELECT * FROM table_metadata WHERE table_name = '" . $table_name . "';")->getResultArray()[0];
+        $table = $db->query("SELECT * FROM table_metadata WHERE table_name = '" . $table_name . "';")->getRowArray();
 
         $show = json_decode($table["show_permissions"]);
         $add = json_decode($table["add_permissions"]);
@@ -103,7 +103,7 @@ class FormFilter implements FilterInterface
     {
         $db = db_connect();
 
-        $column_sql = "SELECT column_name, required FROM public.form_metadata WHERE table_name = '" . $table_name . "' ORDER BY order_position ASC;";
+        $column_sql = "SELECT column_name, required, to_foreign FROM public.form_metadata WHERE table_name = '" . $table_name . "' ORDER BY order_position ASC;";
         $columns_entries = $db->query($column_sql)->getResultArray();
 
         $auth = service('auth');
@@ -114,7 +114,12 @@ class FormFilter implements FilterInterface
         $allowed = [];
 
         foreach ($columns_entries as $j => $column_entry) {
-            $column_metadata = $db->query("SELECT required_permissions FROM public.column_metadata WHERE column_name = '" . $column_entry["column_name"] . "';")->getResultArray()[0];
+            if ($column_entry["to_foreign"] == "t") {
+                array_push($allowed, $column_entry['column_name']);
+                continue;
+            }
+
+            $column_metadata = $db->query("SELECT required_permissions FROM public.column_metadata WHERE table_name = '" . $table_name . "' AND column_name = '" . $column_entry["column_name"] . "';")->getResultArray()[0];
 
             if ($column_entry["required"] == "t" || !$filter->columnPremissionDenied($column_metadata["required_permissions"], $user)) {
                 array_push($allowed, $column_entry['column_name']);
--- public/js/form.js
+++ public/js/form.js
@@ -230,123 +230,137 @@ function form_prepare(f) {
   }
 }
 
-function submit(i, values, extra, fk) {
-  let link = links[i];
-
+function submit(values, extra) {
+  let post = [];
   let formData = new FormData();
 
-  if (fk != null) {
-    formData.append(link.param, fk);
-  }
+  for (let i = 0; i < links.length; i++) {
+    let link = links[i];
 
-  for (let i = 0; i < Object.keys(schema.properties).length; i++) {
-    let key = Object.keys(schema.properties)[i];
+    let table = {
+      name: link.table,
+      data: [],
+    };
 
-    if (link.keys.includes(key)) {
-      if (schema.properties[key].type == "file") {
-        for (let j = 0; j < file_arr.length; j++) {
-          formData.append("files[]", file_arr[j]);
+    for (let i = 0; i < Object.keys(schema.properties).length; i++) {
+      let key = Object.keys(schema.properties)[i];
 
-          formData.append(key, "?filename");
-        }
-      } else if (schema.properties[key].type == "select") {
-        let mult = $('[name="' + key + '"]').attr("multiple");
-
-        if (mult != undefined) {
-          if ($('[name="' + key + '"]').val() != null) {
-            let select_value = $('[name="' + key + '"]')
-              .val()
-              .map(Number);
-            formData.append(key, JSON.stringify(select_value));
+      if (link.keys.includes(key)) {
+        let segment = { key: key, value: [], files: true };
+        if (schema.properties[key].type == "file") {
+          for (let j = 0; j < file_arr.length; j++) {
+            formData.append("files[]", file_arr[j]);
+            segment.value.push(file_arr[j].name);
           }
+          table.data.push(segment);
+        } else if (schema.properties[key].type == "select") {
+          let mult = $('[name="' + key + '"]').attr("multiple");
+
+          if (mult != undefined) {
+            if ($('[name="' + key + '"]').val() != null) {
+              let select_value = $('[name="' + key + '"]')
+                .val()
+                .map(Number);
+              table.data.push({
+                key: key,
+                value: JSON.stringify(select_value),
+              });
+            }
+          } else {
+            let select_value = $('[name="' + key + '"] option:selected').text();
+            table.data.push({ key: key, value: select_value });
+          }
+        } else if (schema.properties[key].type == "array") {
+          table.data.push({ key: key, value: JSON.stringify(values[key]) });
         } else {
-          let select_value = $('[name="' + key + '"] option:selected').text();
-          formData.append(key, select_value);
+          table.data.push({ key: key, value: values[key] });
         }
-      } else if (schema.properties[key].type == "array") {
-        formData.append(key, JSON.stringify(values[key]));
-      } else {
-        let value = values[key];
-        formData.append(key, value);
       }
     }
+
+    post.push(table);
   }
 
-  let save_url = "http://localhost:8080/forms/" + link.table + "/" + link.type;
-  if (link.index != null)
-    save_url =
-      "http://localhost:8080/forms/" +
-      link.table +
-      "/" +
-      link.type +
-      "/" +
-      link.index +
-      extra;
+  console.log(post);
+  // if (fk != null) {
+  //   formData.append(link.param, fk);
+  // }
 
-  $.ajax({
-    url: save_url,
-    type: "post",
-    data: formData,
-    processData: false,
-    contentType: false,
-    success: function (response) {
-      let resp = JSON.parse(response);
-      if (links.length - 1 > i) {
-        let x = "";
-        if (link.type != "add") {
-          x = "/" + links[i + 1].param;
-        }
-        submit(i + 1, values, x, resp.id);
-      }
-      let mesg = document.getElementById("message");
-      mesg.innerHTML = resp.message;
-      mesg.style.color = "green";
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.error(jqXHR);
-      console.error(textStatus);
-      console.error(errorThrown);
+  //   let save_url = "http://localhost:8080/forms/" + link.table + "/" + link.type;
+  //   if (link.index != null)
+  //     save_url =
+  //       "http://localhost:8080/forms/" +
+  //       link.table +
+  //       "/" +
+  //       link.type +
+  //       "/" +
+  //       link.index +
+  //       extra;
 
-      let mesg = document.getElementById("message");
-      mesg.innerHTML = "Someting went wrong.";
-      mesg.style.color = "red";
-    },
-  });
-}
+  //   $.ajax({
+  //     url: save_url,
+  //     type: "post",
+  //     data: formData,
+  //     processData: false,
+  //     contentType: false,
+  //     success: function (response) {
+  //       let resp = JSON.parse(response);
+  //       if (links.length - 1 > i) {
+  //         let x = "";
+  //         if (link.type != "add") {
+  //           x = "/" + links[i + 1].param;
+  //         }
+  //         submit(i + 1, values, x, resp.id);
+  //       }
+  //       let mesg = document.getElementById("message");
+  //       mesg.innerHTML = resp.message;
+  //       mesg.style.color = "green";
+  //     },
+  //     error: function (jqXHR, textStatus, errorThrown) {
+  //       console.error(jqXHR);
+  //       console.error(textStatus);
+  //       console.error(errorThrown);
 
-// Pirms formas noformatēšana=================
-//====================================
-for (let i = 0; i < form.length; i++) {
-  let f = form[i];
+  //       let mesg = document.getElementById("message");
+  //       mesg.innerHTML = "Someting went wrong.";
+  //       mesg.style.color = "red";
+  //     },
+  //   });
+  // }
 
-  if (f.type == "fieldset") {
-    for (let j = 0; j < f.items[0].items.length; j++) {
-      for (let k = 0; k < f.items[0].items[j].items.length; k++) {
-        let n_f = f.items[0].items[j].items[k];
-        form_prepare(n_f);
-      }
-    }
-  } else {
-    form_prepare(f);
-  }
-}
+  // // Pirms formas noformatēšana=================
+  // //====================================
+  // for (let i = 0; i < form.length; i++) {
+  //   let f = form[i];
 
-if (value.length > 0) {
-  for (let i = 0; i < Object.keys(schema.properties).length; i++) {
-    let key = Object.keys(schema.properties)[i];
-    let f = schema.properties[key];
+  //   if (f.type == "fieldset") {
+  //     for (let j = 0; j < f.items[0].items.length; j++) {
+  //       for (let k = 0; k < f.items[0].items[j].items.length; k++) {
+  //         let n_f = f.items[0].items[j].items[k];
+  //         form_prepare(n_f);
+  //       }
+  //     }
+  //   } else {
+  //     form_prepare(f);
+  //   }
+  // }
 
-    if (f.type == "boolean") {
-      let flag = value[0][key];
+  // if (value.length > 0) {
+  //   for (let i = 0; i < Object.keys(schema.properties).length; i++) {
+  //     let key = Object.keys(schema.properties)[i];
+  //     let f = schema.properties[key];
 
-      console.log(flag);
-      if (flag == "t") {
-        value[0][key] = true;
-      } else if (flag == "f") {
-        value[0][key] = false;
-      }
-    }
-  }
+  //     if (f.type == "boolean") {
+  //       let flag = value[0][key];
+
+  //       console.log(flag);
+  //       if (flag == "t") {
+  //         value[0][key] = true;
+  //       } else if (flag == "f") {
+  //         value[0][key] = false;
+  //       }
+  //     }
+  //   }
 }
 
 //Formas ģenerēšana ==============================
@@ -356,7 +370,7 @@ $("#test-form").jsonForm({
   form: form,
   value: value[0],
   onSubmitValid: function (values) {
-    submit(0, values, "", null);
+    submit(values, "");
   },
 });
 
